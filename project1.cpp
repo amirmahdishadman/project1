4e5115cc285d9e7057fcb8286cf92f51772b9139
Mahdi <amirmahdishadman98@gmail.com>
2020-01-03 14:07:58 +0330

    fixed family section

--- project1.cpp
+++ project1.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main()
 {
     char fullname[] ={'m','a','h','d','i'};
+    char family[] ={'m','a','h','d','i'};
+
    for(int i=0;i<5;i++){
     cout<<fullname[i];
+    cout<<family[i];
 }
 }
